12016394d4c2ec50b788cc21dccdd36e04febe4c
Zach Hannum <zacharyhannum@gmail.com>
2022-04-15 18:31:21 -0400

    Fixes

--- app/components/writer.tsx
+++ app/components/writer.tsx
@@ -1,22 +1,41 @@
-// import React from 'react';
-// import { Editor, EditorState } from 'draft-js';
-// import 'draft-js/dist/Draft.css';
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import styled from '@emotion/styled';
+import { Editor, EditorState } from 'draft-js';
+import 'draft-js/dist/Draft.css';
 
+const ScrollContainer = styled.div`
+  overflow-y: auto;
+  height: 100%;
+  width: 100%;
+  display: flex;
+`;
+
+const WriterContainer = styled.div`
+  height: 90%;
+  width: 90%;
+`;
 
 const Writer = () => {
-  const WriterContainer = styled.div`
-    max-width: 500px;
-    margin: auto;
-  `;
-  const [isActive, toggleActive] = useState(false);
-  // const [editorState, setEditorState] = React.useState(() =>
-  //   EditorState.createEmpty()
-  // );
+  const [editorState, setEditorState] = React.useState(() =>
+    EditorState.createEmpty()
+  );
+
+  const editorRef = useRef<Editor>(null);
+
+  const setFocus = () => {
+    editorRef.current?.focus();
+  };
 
   return (
-      <WriterContainer>Hello</WriterContainer>
+    <ScrollContainer>
+      <WriterContainer onClick={() => setFocus()}>
+        <Editor
+          editorState={editorState}
+          onChange={setEditorState}
+          ref={editorRef}
+        />
+      </WriterContainer>
+    </ScrollContainer>
   );
 };
 
