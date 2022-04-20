3d9207f8859e5ff337d88bfc80c781c44169648e
Zach Hannum <zacharyhannum@gmail.com>
2022-04-20 01:20:11 -0400

    Added default theme

--- app/renderer/App.tsx
+++ app/renderer/App.tsx
@@ -1,8 +1,9 @@
 // import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import Writer from './components/Writer';
 import ProjectSidebar from './components/ProjectSidebar';
+import theme from './theme/theme';
 
 const AppContainer = styled.div`
   display: flex;
@@ -17,16 +18,20 @@ const MainContent = styled.div`
   padding-top: env(titlebar-area-height, 500px);
   width: 100%;
   height: calc(100% - env(titlebar-area-height, 500px));
+  color: ${(p) => p.theme.mainFgText};
+  background-color: ${(p) => p.theme.mainBg};
 `;
 
 const App = () => {
   return (
-    <AppContainer>
-      <ProjectSidebar />
-      <MainContent>
-        <Writer />
-      </MainContent>
-    </AppContainer>
+    <ThemeProvider theme={theme}>
+      <AppContainer>
+        <ProjectSidebar />
+        <MainContent>
+          <Writer />
+        </MainContent>
+      </AppContainer>
+    </ThemeProvider>
   );
 };
 
