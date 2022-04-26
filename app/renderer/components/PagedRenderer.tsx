52ce6b96f3a8bdb18c2024fb0e4214a4186d09e3
Zach Hannum <zacharyhannum@gmail.com>
2022-04-26 16:05:47 -0400

    Add print font size demo

--- app/renderer/components/PagedRenderer.tsx
+++ app/renderer/components/PagedRenderer.tsx
@@ -4,12 +4,13 @@ import { Polisher, Chunker, initializeHandlers } from 'pagedjs';
 import { baseStylesheet } from '../pagedjs/defaultPageCss';
 import { TestContent } from '../pagedjs/pagedTestContent';
 import { useResizeObserver } from '../hooks';
-// import useStore from '../store/useStore';
+import useStore from '../store/useStore';
 
 const StyledRenderer = styled.div`
   height: 70%;
   width: 70%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
@@ -45,18 +46,24 @@ type PagedRendererProps = {
 const PagedRenderer = ({ pageNumber, onPageOverflow }: PagedRendererProps) => {
   const pageContainerRef = useRef<HTMLDivElement>(null);
   const rendererRef = useRef<HTMLDivElement>(null);
-  // const previewEnabled = useStore((state) => state.previewEnabled);
+  /* Testing dynamic css changes */
+  const printParagraphFontSize = useStore(
+    (state) => state.printParagraphFontSize
+  );
+  const setPrintParagraphFontSize = useStore(
+    (state) => state.setPrintParagraphFontSize
+  );
   const polisher = useRef<Polisher>(null);
   const chunker = useRef<Chunker>(null);
   const [scale, setScale] = useState<number>(0.5);
   const [page, setPage] = useState(1);
 
-  const navigateToPage = (newPage: number) => {
+  const navigateToPage = (newPage: number, instant = false) => {
     const navigatePage = document.querySelector(
       `[data-page-number="${newPage}"]`
     );
     if (navigatePage) {
-      navigatePage.scrollIntoView({ behavior: 'smooth' });
+      navigatePage.scrollIntoView({ behavior: instant ? 'auto' : 'smooth' });
       setPage(newPage);
     } else {
       onPageOverflow(page);
@@ -94,8 +101,14 @@ const PagedRenderer = ({ pageNumber, onPageOverflow }: PagedRendererProps) => {
       if (polisher.current && chunker.current) {
         polisher.current.setup();
         initializeHandlers(chunker.current, polisher.current);
-        await polisher.current.add({ '': baseStylesheet.toString() });
+        await polisher.current.add({
+          '': baseStylesheet({
+            paragraphFontSize: printParagraphFontSize,
+          }).toString(),
+        });
         chunker.current.flow(template.content, container);
+        setPage(1);
+        onPageOverflow(1);
       }
     }
   };
@@ -103,13 +116,36 @@ const PagedRenderer = ({ pageNumber, onPageOverflow }: PagedRendererProps) => {
   useEffect(() => {
     init();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [printParagraphFontSize]);
+
+  const handleFontChange = (increase: boolean) => {
+    const newFontSize = increase
+      ? printParagraphFontSize + 1
+      : printParagraphFontSize - 1;
+    setPrintParagraphFontSize(newFontSize);
+  };
 
   return (
     <StyledRenderer ref={rendererRef}>
       <Scaler scale={scale}>
         <PageContainer ref={pageContainerRef} />
       </Scaler>
+      <button
+        type="button"
+        onClick={() => {
+          handleFontChange(true);
+        }}
+      >
+        Font +
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          handleFontChange(false);
+        }}
+      >
+        Font -
+      </button>
     </StyledRenderer>
   );
 };
