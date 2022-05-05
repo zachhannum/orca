2c080568690111f796f974d20c36a394a85bcb3b
Zach Hannum <zacharyhannum@gmail.com>
2022-05-05 09:10:32 -0400

    Disable drag when item is editable

--- app/renderer/components/TreeView/components/TreeItem/TreeItem.tsx
+++ app/renderer/components/TreeView/components/TreeItem/TreeItem.tsx
@@ -1,4 +1,10 @@
-import React, { forwardRef, HTMLAttributes, useState } from 'react';
+import React, {
+  forwardRef,
+  HTMLAttributes,
+  useState,
+  useRef,
+  useEffect,
+} from 'react';
 import styled, { css } from 'styled-components';
 import Color from 'color';
 import { IconButton } from 'renderer/controls';
@@ -47,12 +53,6 @@ const StyledTreeItem = styled.div<StyledTreeItemProps>`
   border-radius: 5px;
 
   ${(p) =>
-    p.contentEditable &&
-    css`
-      background-color: ${(p) => Color(p.theme.sidebarBg).darken(0.2)};
-    `}
-
-  ${(p) =>
     p.clone &&
     css`
       margin-top: 20px;
@@ -87,6 +87,13 @@ const StyledTreeItem = styled.div<StyledTreeItemProps>`
           : Color(p.theme.sidebarBg).lighten(0.3)};
       }
     `}
+
+    ${(p) =>
+    p.isEditable &&
+    css`
+      cursor: text;
+      background-color: ${(p) => Color(p.theme.sidebarBg).darken(0.2)};
+    `}
 `;
 
 const StyledText = styled.span`
@@ -96,24 +103,8 @@ const StyledText = styled.span`
   text-overflow: ellipsis;
   overflow: hidden;
   :focus {
-        outline: none;
-      }
-`;
-
-const StyledCount = styled.span`
-  position: absolute;
-  top: -10px;
-  left: -10px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 24px;
-  height: 24px;
-  border-radius: 50%;
-  background-color: #2389ff;
-  font-size: 0.8rem;
-  font-weight: 600;
-  color: #fff;
+    outline: none;
+  }
 `;
 
 export interface Props extends HTMLAttributes<HTMLLIElement> {
@@ -156,12 +147,47 @@ export const TreeItem = forwardRef<HTMLDivElement, Props>(
     ref
   ) => {
     const [isEditable, setIsEditable] = useState(false);
+    const [dragProps, setDragProps] = useState({ ...handleProps });
+    const textRef = useRef<HTMLSpanElement>();
     const handleDoubleClick = () => {
       setIsEditable(true);
       setTimeout(() => {
-        // itemRef.current?.focus();
+        textRef.current?.focus();
       }, 10);
     };
+    const handleBlur = () => {
+      setIsEditable(false);
+
+      // const newValue = textRef.current?.innerText;
+      // if (newValue) updateSectionName(value, newValue);
+    };
+    // useEffect(() => {
+    //   if (addingNew) {
+    //     setIsEditable(true);
+    //     textRef.current?.scrollTo({ behavior: 'smooth' });
+    //     setTimeout(() => {
+    //       textRef.current?.focus();
+    //     });
+    //   }
+    // }, []);
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLSpanElement>) => {
+      if (event.code === 'Enter') {
+        event.preventDefault();
+        handleBlur();
+      } else if (event.code === 'Escape') {
+        event.preventDefault();
+        if (textRef.current) textRef.current.innerText = value;
+        setIsEditable(false);
+      }
+    };
+    useEffect(() => {
+      if (isEditable) {
+        setDragProps({});
+      } else {
+        setDragProps({ ...handleProps });
+      }
+    });
+
     return (
       <StyledTreeItemWrapper
         clone={clone}
@@ -190,20 +216,20 @@ export const TreeItem = forwardRef<HTMLDivElement, Props>(
               <PlusIcon />
             </IconButton>
           )}
-          <StyledText {...handleProps} onDoubleClick={handleDoubleClick} contentEditable={isEditable}>
+          <StyledText
+            {...dragProps}
+            onDoubleClick={handleDoubleClick}
+            contentEditable={isEditable}
+            disabled={isEditable}
+            suppressContentEditableWarning
+            onBlur={handleBlur}
+            onKeyDown={handleKeyDown}
+            ref={textRef}
+          >
             {value}
           </StyledText>
-          {clone && childCount && childCount > 1 ? (
-            <StyledCount>{childCount}</StyledCount>
-          ) : null}
         </StyledTreeItem>
       </StyledTreeItemWrapper>
     );
   }
-);
-
-const collapseIcon = (
-  <svg width="10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70 41">
-    <path d="M30.76 39.2402C31.885 40.3638 33.41 40.995 35 40.995C36.59 40.995 38.115 40.3638 39.24 39.2402L68.24 10.2402C69.2998 9.10284 69.8768 7.59846 69.8494 6.04406C69.822 4.48965 69.1923 3.00657 68.093 1.90726C66.9937 0.807959 65.5106 0.178263 63.9562 0.150837C62.4018 0.123411 60.8974 0.700397 59.76 1.76024L35 26.5102L10.24 1.76024C9.10259 0.700397 7.59822 0.123411 6.04381 0.150837C4.4894 0.178263 3.00632 0.807959 1.90702 1.90726C0.807714 3.00657 0.178019 4.48965 0.150593 6.04406C0.123167 7.59846 0.700153 9.10284 1.75999 10.2402L30.76 39.2402Z" />
-  </svg>
 );
