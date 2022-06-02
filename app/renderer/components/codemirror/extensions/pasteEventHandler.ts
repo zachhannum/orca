adf1128c05789a80ad71ca364990437a91c2bea3
Zach Hannum <zacharyhannum@gmail.com>
2022-06-02 11:39:28 -0400

    First pass at paste event handler to translate to markdown on paste

--- app/renderer/components/codemirror/extensions/pasteEventHandler.ts
+++ app/renderer/components/codemirror/extensions/pasteEventHandler.ts
@@ -1,13 +1,35 @@
-import { Extension } from '@codemirror/state'
-import { EditorView } from '@codemirror/view'
+import { Extension } from '@codemirror/state';
+import { EditorView } from '@codemirror/view';
+import { unified } from 'unified';
+import rehypeParse from 'rehype-parse';
+import rehypeRemark from 'rehype-remark';
+import remarkStringify from 'remark-stringify';
+import rehypeRemoveComments from 'rehype-remove-comments';
 
 const pasteEventHandler = (): Extension => {
   return EditorView.domEventHandlers({
     paste(event: ClipboardEvent, view: EditorView) {
-        // TODO. Parse HTML to markdown and paste
-        // console.log(event.clipboardData?.getData('text/html'));
-    }
-  })
-}
+      const html = event.clipboardData?.getData('text/html');
+      if (html) {
+        event.preventDefault();
+        const markdown = unified()
+          .use(rehypeParse, { fragment: true })
+          .use(rehypeRemoveComments)
+          .use(rehypeRemark)
+          .use(remarkStringify)
+          .processSync(html).value.toString();
+        view.dispatch({
+          changes: {
+            from: view.state.selection.main.from,
+            to: view.state.selection.main.to,
+            insert: markdown,
+          },
+          selection: { anchor: view.state.selection.main.from + markdown.length - 1},
+          scrollIntoView: true,
+        });
+      }
+    },
+  });
+};
 
 export default pasteEventHandler;
